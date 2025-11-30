34a9cf6692c4774ff3af6e7f19d9683658f376d3
George Williams <ghwilli@localhost.localdomain>
2025-11-30 14:55:53 -0700

    testing google on app

--- flaskr/google.py
+++ flaskr/google.py
@@ -1,4 +1,8 @@
 import os
+import jwt
+ 
+from urllib.parse import quote
+from datetime import datetime, timezone, timedelta
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
@@ -28,29 +32,38 @@ def register_google_oauth():
 def setup_oauth():
     register_google_oauth()
 
-@bp.route('/google/')
-def google():
-    redirect_uri = url_for('google.google_auth', _external=True)
+@bp.route('/portal')
+def portal_google():
+    redirect_uri = url_for('google.portal_google_auth', _external=True)
     nonce = os.urandom(16).hex()
     session['nonce'] = nonce
     return oauth.google.authorize_redirect(redirect_uri, nonce=nonce)
 
-@bp.route('/google/auth/')
-def google_auth():
+@bp.route('/portal/auth/')
+def portal_google_auth():
     token = oauth.google.authorize_access_token()
     nonce = session.pop('nonce', None)
-    google_user = oauth.google.parse_id_token(token, nonce)
-    username = f'google_user:{encrypt_id(int(google_user['sub']))}'
-    db = get_db()
     error = None
-    user = db.execute(
-        'SELECT * FROM user WHERE username = ?', (username, )
-    ).fetchone()
+    if nonce is None:
+        error = 'Invalid session (nonce missing)'
 
-    if user is None:
-        error = 'No google account'
-        print(error)
-        print('username', username)
+    if error is None:
+        try:
+            google_user = oauth.google.parse_id_token(token, nonce)
+            username = f"google_user:{encrypt_id(int(google_user['sub']))}"
+            db = get_db()
+            error = None
+            user = db.execute(
+                'SELECT * FROM user WHERE username = ?', (username, )
+            ).fetchone()
+
+            print(google_user)
+            if user is None:
+                error = 'No google account'
+                print(error)
+                print('username', username)
+        except Exception as e:
+            error = f'Invalid google user: {e}'
 
     if error is None:
         session.clear()
@@ -59,3 +72,45 @@ def google_auth():
 
     flash(error)
     return redirect(url_for('portal.login'))
+
+@bp.route('/android')
+def android_google():
+    redirect_uri = url_for('google.android_google_auth', _external=True)
+    nonce = os.urandom(16).hex()
+    session['nonce'] = nonce
+    return oauth.google.authorize_redirect(redirect_uri, nonce=nonce)
+
+@bp.route('/android/auth/')
+def android_google_auth():
+    token = oauth.google.authorize_access_token()
+    nonce = session.pop('nonce', None)
+    error = None
+    if nonce is None:
+        error = 'Invalid session (nonce missing)'
+
+    if error is None:
+        try:
+            google_user = oauth.google.parse_id_token(token, nonce)
+            google_id =encrypt_id(int(google_user['sub']))
+            email = google_user['email']
+            name = google_user['name']
+
+            JWT_SECRET = os.getenv('JWT_SECRET')
+            now = datetime.now(timezone.utc)
+            payload = {
+                'sub': google_id,
+                'name': name,
+                'email': email,
+                'iat': now,
+                'exp': now + timedelta(minutes=15)
+            }
+            jwt_token = jwt.encode(payload, JWT_SECRET, algorithm='HS256')
+            print(jwt_token)
+        except Exception as e:
+            error = f'Invalid google login: {e}'
+
+    if error is None:
+        return redirect(f"gymmateapp://auth?token={jwt_token}")
+    
+    print(error)
+    return redirect(f"gymmateapp://auth?error={quote(error)}")
